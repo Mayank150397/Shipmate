ca61ed0f0137e8c94518f1bea9cbadc528a916db
Mayank150397 <36702470+Mayank150397@users.noreply.github.com>
2023-04-26 09:11:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Front from "./components/front.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Front />
+    </div>
+  );
+}
---
+++ src/components/front.js
@@ -0,0 +1,12 @@
+import "./front.css";
+
+export default function Front() {
+  return (
+    <div className="Front">
+      <header className="Header">
+        <h1 className="Ship">Shipmate</h1>
+        <button className="login">Login</button>
+      </header>
+    </div>
+  );
+}
